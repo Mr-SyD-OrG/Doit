611b544adbce421a0c297dfe5745416f4fbd96aa
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-02-22 20:55:38 +0530

    Update forward.py

--- Syd/forward.py
+++ Syd/forward.py
@@ -87,7 +87,7 @@ async def handle_message(event):
                     try:
                         await event.message.click(row_idx, col_idx)  # Correct way to click
                         print(f"Pressed: {button.text}")
-                        await asyncio.sleep(15)  # 15-second delay between presses
+                        await asyncio.sleep(65)  # 15-second delay between presses
                     except Exception as e:
                         print(f"Error pressing button {button.text}: {e}")
 
