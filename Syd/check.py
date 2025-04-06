a149f56f1ad5a63f1b39594adf6a3ed6cddfafad
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-06 12:35:58 +0530

    Update check.py

--- Syd/check.py
+++ Syd/check.py
@@ -5,17 +5,6 @@ from bot import mrsyd
 
 target_users = [6592320604]
 # Define your handler
-@mrsyd.on(events.NewMessage(from_users=[7065204410], pattern=r"^Check"))
-async def trigger(event):
-    await event.respond("Starting the daily /start loop.")
-
-    while True:
-        for user_id in target_users:
-            await mrsyd.send_message(user_id, '/start')
-            await asyncio.sleep(60)  # Delay between each message
-        #await asyncio.sleep(86400)
-        await asyncio.sleep(8)# Wait for a day before repeating
- # Replace with actual user IDs
 
 # Global response tracker
 response_received = asyncio.Event()
