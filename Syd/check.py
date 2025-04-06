923a4237bbb239589e4791d84cf2ae64e67bc48a
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-06 12:31:28 +0530

    Update check.py

--- Syd/check.py
+++ Syd/check.py
@@ -2,6 +2,8 @@ from telethon import events
 import asyncio
 from bot import mrsyd
 
+
+target_users = [6592320604]
 # Define your handler
 @mrsyd.on(events.NewMessage(from_users=[7065204410], pattern=r"^Check"))
 async def trigger(event):
@@ -11,4 +13,5 @@ async def trigger(event):
         for user_id in target_users:
             await mrsyd.send_message(user_id, '/start')
             await asyncio.sleep(60)  # Delay between each message
-        await asyncio.sleep(86400)  # Wait for a day before repeating
+        #await asyncio.sleep(86400)
+        await asyncio.sleep(8)# Wait for a day before repeating
