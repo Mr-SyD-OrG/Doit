9c6ee68ab5fe0c85e22e30494928af2f2c54d748
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-11 10:38:12 +0530

    Update mrsyd.py

--- Syd/mrsyd.py
+++ Syd/mrsyd.py
@@ -1,22 +1,24 @@
 from telethon import events
 import asyncio
+from bot import mrsyd
+
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 start_year = 2000
-end_year = 2025
+end_year = 2004
 resolutions = ["240p", "480p", "720p", "1080p", "2160p"]
 
-@mrsyd.on(events.NewMessage(from_users=[197065204410], pattern=r"Search"))
+@mrsyd.on(events.NewMessage(from_users=[1733124290], pattern=r"Search"))
 async def handle_search_trigger(event):
     chat_id = event.chat_id
-    await event.reply("Starting resolution spam...")
+    await event.reply("Starting resolution.")
 
     for i, year in enumerate(range(start_year, end_year + 1)):
         letter = letters[i % len(letters)]
         for res in resolutions:
             msg = f"{letter} {year} {res}"
-            await event.client.send_message(chat_id, msg)
+            await event.client.send_message(1983814301, msg)
             print(f"Sent: {msg}")
-            await asyncio.sleep(60)  # 1 minute delay
+            await asyncio.sleep(600)  # 1 minute delay
 
     await event.client.send_message(chat_id, "Done sending all resolutions.")
