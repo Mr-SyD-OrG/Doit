1474e76c627ad77709653c7386e78e1fd2d3b81f
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-14 07:32:54 +0530

    Update mrsyd.py

--- Syd/mrsyd.py
+++ Syd/mrsyd.py
@@ -7,6 +7,7 @@ import random
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 start_year = 2000
 end_year = 2002
+messge = ["A 2000 480p", "A 2000 720p", "A 2000 1080p", "B 2001 480p", "B 2001 720p", "B 2001 1080p", "C 2002 480p", "C 2002 720p", "C 2002 1080p"]
 resolutions = ["240p", "480p", "720p", "1080p", "2160p"]
 WAIT = [35, 120, 240, 300, 360, 420, 540, 600, 700, 800, 1000, 1200, 3000]
 @mrsyd.on(events.NewMessage(from_users=[1733124290], pattern=r"Search"))
@@ -17,8 +18,9 @@ async def handle_search_trigger(event):
         for year in range(start_year, end_year + 1):
             for res in resolutions:
                 msg = f"{letter} {year} {res}"
-                await event.client.send_message(1983814301, msg)
-                print(f"Sent: {msg}")
+                if msg not in messge:
+                    await event.client.send_message(1983814301, msg)
+                    print(f"Sent: {msg}")
                 await asyncio.sleep(random.choice(WAIT))
 
     await event.client.send_message(1733124290, "Done sending all resolutions.")
