fd6b618dc337579c41f31727b9d3ec7ae2d70f1d
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-19 07:46:03 +0530

    Update forward.py

--- Syd/forward.py
+++ Syd/forward.py
@@ -19,7 +19,7 @@ semaphore = Semaphore(2)
 semapore = asyncio.Semaphore(1)
 #DESTINATION_CHAT = [-1002536001013, -1002523513653]
 DESTINATION_CHATS = [-1002433450358, -1002464733363, -1002280144341, -1002429058090]
-SOURCE_CHATS = [-1002295881345, 8161322603, -1002525578839, 7065204410, 7519971717, 8128434604, 1983814301, 7755788244, 7193976370, -1001780243928, -1001862599580, -1002077435396]
+SOURCE_CHATS = [-1002295881345, 7885202047, -1002525578839, 7065204410, 7519971717, 8128434604, 1983814301, 7755788244, 7193976370, -1001780243928, -1001862599580, -1002077435396]
 
 
 @mrsyd.on(events.NewMessage(chats=SOURCE_CHATS, func=lambda e: e.message.media and (e.message.video or e.message.document)))
@@ -79,7 +79,7 @@ async def handle_new_source(event):
 
 
 
-@mrsyd.on(events.NewMessage(from_users=[8161322603, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^🔍 Results for your Search"))
+@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^🔍 Results for your Search"))
 async def syde_message(event):
     """Press each button every 60 seconds until a new message arrives, then move to the next button.
     'NEXT' is only pressed at the end, followed by a elay before fetching new buttons.
@@ -161,7 +161,7 @@ async def syde_message(event):
 
 
 
-@mrsyd.on(events.NewMessage(from_users=[8161322603, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^❗️Join"))
+@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^❗️Join"))
 async def handle_invite(event):
     """Click the first inline button if it's an invite link and request to join."""
     message = event.message
