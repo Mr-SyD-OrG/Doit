91b74936747f9e43462a0f03b28d08d69552f07e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-19 08:02:47 +0530

    Update forward.py

--- Syd/forward.py
+++ Syd/forward.py
@@ -79,7 +79,7 @@ async def handle_new_source(event):
 
 
 
-@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^🔍 Results for your Search"))
+@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7519971717, 8128434604], pattern=r"^🔍 Results for your Search"))
 async def syde_message(event):
     """Press each button every 60 seconds until a new message arrives, then move to the next button.
     'NEXT' is only pressed at the end, followed by a elay before fetching new buttons.
@@ -161,7 +161,7 @@ async def syde_message(event):
 
 
 
-@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7065204410, 7519971717, 8128434604], pattern=r"^❗️Join"))
+@mrsyd.on(events.NewMessage(from_users=[7885202047, 1983814301, 7755788244, 7519971717, 8128434604], pattern=r"^❗️Join"))
 async def handle_invite(event):
     """Click the first inline button if it's an invite link and request to join."""
     message = event.message
