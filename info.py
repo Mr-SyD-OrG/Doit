b0d487cc8ba01a7fc74ac527ecc5e4bbff60645f
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-10-01 12:47:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,6 +14,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Mrsyd")
 DATABASE_URL = environ.get('DATABASE_URL', "")
 API_ID = environ.get('API_URL', "")
 API_HASH = environ.get('API_URI', "")
+TELETHON_SESSION = environ.get('TELETHON_SESSION', "")
 PHONE_NUMBER = environ.get('NUMB', "")
 SOURCE_CHAT_ID = -1002295881345  # Replace with source chat ID
 DESTINATION_CHAT_ID = -1002377676305
