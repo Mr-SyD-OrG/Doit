641f64aba321552825b97d2a0c7a8d4973c4f190
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2026-05-17 18:56:08 +0530

    Remove admin notification in solve_robot_check
    Removed the send_message call to ADMIN_ID in solve_robot_check function.

--- Syd/mrsyd.py
+++ Syd/mrsyd.py
@@ -986,10 +986,6 @@ ADMIN_ID = 1733124290
     )
 )
 async def solve_robot_check(event):
-    await mrsyd.send_message(
-                            ADMIN_ID,
-                            f"✅ n"
-    )
     message = event.message
     logging.info(message)
 
