a17b604003d0724ec9b5b3dc6b8d3c165ee0187b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2026-05-19 09:27:29 +0530

    Update web.py

--- Syd/web.py
+++ Syd/web.py
@@ -172,7 +172,7 @@ async def open_real(url):
         await page.goto(
             url,
             wait_until="commit",
-            timeout=3500
+            timeout=9500
         )
 
         # reading time
