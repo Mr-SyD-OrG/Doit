b8981d0317941cea399f41722a79c852a5bf54e0
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2026-06-11 08:17:28 +0530

    Enhance limit message with click count
    Added click count to limit message reply.

--- Syd/mrsyd.py
+++ Syd/mrsyd.py
@@ -2327,7 +2327,7 @@ async def click_loop(msg_id, event, ttl=30):
 
                                 click_count += 1
                                 clicked = True
-                                if psyd.message and "Ты слишком" in psyd.message: await event.reply(f"Approached Limit \n {psyd}")
+                                if psyd.message and "Ты слишком" in psyd.message: await event.reply(f"Approached Limit \n {psyd}\n 🌱: {click_count}")
 
                                 logging.info(
                                     f"✅ Clicked {click_count}/30 {psyd}"
