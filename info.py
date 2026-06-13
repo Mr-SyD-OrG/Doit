a63dff61e393217f19250d37b6c7f3a20557ad50
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2026-06-13 10:19:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,15 +12,13 @@ def is_enabled(value, default):
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Mrsyd")
 DATABASE_URL = environ.get('DATABASE_URL', "")
-API_ID1 = environ.get('API_URL', "")
-API_HASH1 = environ.get('API_URI', "")
-API_ID2 = environ.get('API_URL2', "")
-API_HASH2 = environ.get('API_URI2', "")
+API_ID = environ.get('API_URL', "")
+API_HASH = environ.get('API_URI', "")
 TELETHON_SESSION = environ.get('TELETHON_SESSION', "")
-PHONE_NUMBER1 = environ.get('NUMB', "")
-PHONE_NUMBER2 = environ.get('NUMB2', "")
+PHONE_NUMBER = environ.get('NUMB', "")
 VSYD = environ.get("VSYD", "True").lower() == "true"
 SOURCE_CHAT_ID = -1002295881345  # Replace with source chat ID
 DESTINATION_CHAT_ID = -1002377676305
 auth_channel = environ.get('AUTH_CHANNEL', '')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
